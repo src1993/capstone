000b4b6666cf1f559adcf0bdf9e994dd4d2510cd
Lore <lors2404@gmail.com>
2017-02-12 14:39:35 -0500

    events

--- exploratory.R
+++ exploratory.R
@@ -5,9 +5,12 @@ setwd("/Users/loredp/Dropbox (MIT)/MIT/Capstone")
 
 #Read/load data
 
-events <- read.csv('data/events.csv')
+events <- read.csv('data/events.csv', as.is = T)
 fiscal <- read.csv('data/fscl.csv')
 hier <- read.csv('data/hier.csv')
 catalog <- read.csv('data/pctlg_sku.csv')
-price <- load('data/price_type.rda')
+load('data/price_type.rda')
 tran <- read.csv('data/tran.csv')
+
+#EVENTS
+
