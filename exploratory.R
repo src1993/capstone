d2a30688e247e133933ade1b16f2edb82e89af31
Lore <lors2404@gmail.com>
2017-02-14 11:40:27 -0500

    hierarchy

--- exploratory.R
+++ exploratory.R
@@ -5,13 +5,14 @@ setwd("/Users/loredp/Dropbox (MIT)/MIT/Capstone")
 
 #Libraries
 library(dplyr)
+library(ggplot2)
 
 #Read/load data
 
-events <- read.csv('data/events.csv', as.is = T)
-# fiscal <- read.csv('data/fscl.csv')
+#events <- read.csv('data/events.csv', as.is = T)
+#fiscal <- read.csv('data/fscl.csv', as.is = T)
 # hier <- read.csv('data/hier.csv')
-# catalog <- read.csv('data/pctlg_sku.csv')
+# catalog <- read.csv('data/pctlg_sku.csv', as.is = T)
 # load('data/price_type.rda')
 # tran <- read.csv('data/tran.csv')
 
@@ -24,5 +25,19 @@ events <- events %>%
            format = "%m/%d/%Y"),
            duration = end-start+1) %>%
     select(-c(DT_BEG_ACT,DT_END_ACT))
+
+#FISCAL
+
+fiscal <- fiscal %>%
+    mutate(DATE = as.Date(DAY_DT)) %>%
+    select(-DAY_DT)
+
+#CATALOG
+
+catalog <- catalog %>%
+    mutate(CRT_TMSTP = as.Date(CRT_TMSTP),
+           RCD_UPDT_TMSTP = as.POSIXct(RCD_UPDT_TMSTP))
+
+#PRICE
 
 
