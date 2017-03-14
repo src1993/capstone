4dff68a9ee201ed3f1591ddc902bff9721eaa0a5
Lore <lors2404@gmail.com>
2017-03-14 00:00:42 -0400

    exploration

--- exploratory.R
+++ exploratory.R
@@ -8,6 +8,8 @@ library(dplyr)
 library(ggplot2)
 library(zoo)
 library(lubridate)
+library(rpart)
+library(rpart.plot)
 
 #Read/load data
 
@@ -40,16 +42,13 @@ fiscal <- fiscal %>%
 #HIERARCHY
 
 hier <- hier %>%
-    select(-c(DIV_IDNT,DIV_DESC,SKU_DESC,STYLE_DESC))
-
-table(hier$DM_RECD_CLOSE_DT)
-
+    select(SKU_IDNT,SUPP_NAME, SUPP_NPG_CDE, BRAND_NAME, SBCLASS_DESC, SBCLASS_IDNT,
+           CLASS_DESC, CLASS_IDNT, DEPT_DESC, DEPT_IDNT)
 
 #CATALOG
 
 catalog <- catalog %>%
-    mutate(CRT_TMSTP = as.Date(CRT_TMSTP),
-           RCD_UPDT_TMSTP = as.POSIXct(RCD_UPDT_TMSTP))
+    select(-c(CRT_TMSTP, RCD_UPDT_TMSTP))
 
 #PRICE
 
@@ -58,39 +57,80 @@ price_type <- price_type %>%
 
 #TRANSACTIONS
 
+SKU <- unique(tran$SKU_IDNT)
+sum(!SKU %in% hier$SKU_IDNT) #all apear in hierarchy
+sum(!SKU %in% catalog$SKU_IDNT) #21 don't apear in hierarchy
+
 tran <- tran %>%
     mutate(DAY_DT = as.Date(TRAN_DT),
            UNITS = as.numeric(UNITS),
            DEMAND = as.numeric(DEMAND),
            RTRN_UNITS = as.numeric(RTRN_UNITS),
            RTRN_AMT = as.numeric(RTRN_AMT),
-           UNIT_PRICE = DEMAND/UNITS) %>%
+           UNIT_PRICE_DEMAND = DEMAND/UNITS,
+           UNIT_PRICE_RETURN = -RTRN_AMT/RTRN_UNITS) %>%
     select(-TRAN_DT)
 
-sum(is.na(tran$UNITS)) #n_demand_none 
+# Create product description
+
+description <- hier %>%
+    left_join(catalog, by = "SKU_IDNT")
+
+
 
 #Separate in demand and returns
 
-p <- tran%>%
-    mutate(UNITS+ RTRN_UNITS)%>%
-    arrange(-UNITS+ RTRN_UNITS)
-
 demand <- tran %>%
     filter(!is.na(UNITS)) %>%
-    select(DAY_DT, SKU_IDNT, UNITS, DEMAND, UNIT_PRICE) 
+    mutate(DATE = as.yearmon(DAY_DT)) %>%
+    select(DATE, DAY_DT, SKU_IDNT, UNITS, DEMAND, UNIT_PRICE_DEMAND)
 
 returns <- tran %>%
     filter(!is.na(RTRN_UNITS)) %>%
-    select(DAY_DT, SKU_IDNT, UNITS, DEMAND, UNIT_PRICE) 
+    mutate(DATE = as.yearmon(DAY_DT)) %>%
+    select(DATE, DAY_DT, SKU_IDNT, UNITS, DEMAND, UNIT_PRICE_RETURN) 
 
 #Analyze demand
 
 demand_sku <- demand %>%
-    group_by(SKU_IDNT) %>%
+    group_by(SKU_IDNT, DATE) %>%
     summarize(total_units = sum(UNITS), total_demand = sum(DEMAND))%>%
-    arrange(-total_units)
+    arrange(-total_units) %>%
+    inner_join(description, by = "SKU_IDNT")
 
 summary(demand_sku)
+
+
+####
+
+bridge <- demand_sku %>%
+    filter(DEPT_IDNT == 4) 
+
+contemporary <- demand_sku %>%
+    filter(DEPT_IDNT == 470) 
+
+b_tree1 <- rpart(total_units ~ month(DATE) + BRAND_NAME + SUPP_NPG_CDE + CLASS_DESC + CLR_FMLY_DESC, data = bridge, method = 'anova')
+printcp(b_tree1)
+plotcp(b_tree1, compress = TRUE) 
+plot(b_tree1, uniform = TRUE)
+text(b_tree1, use.n=TRUE, all=TRUE, cex=.8)
+summary(b_tree1)
+prp(b_tree1,type=3)
+
+
+###
+
+trial <- demand_sku %>%
+    group_by(CLASS_DESC) %>%
+    summarize(total_units = sum(total_units), skus = n())
+summary(trial)
+ggplot(trial, aes(x=1, y=skus))+geom_boxplot()
+
+ggplot(demand_sku, aes(x=CLASS_DESC, y=total_units))+geom_boxplot()+ylim(0,200)
+
+
+ggplot(demand_sku, aes(x=SUPP_NPG_CDE, y=total_units))+geom_boxplot()+ylim(0,500)
+
 
 #Demand by date
 
@@ -184,13 +224,52 @@ tran_cat_hor <- tran_cat %>%
 #####
 
 color <- hier %>%
-    group_by(COLR_IDNT, COLR_DESC) %>%
+    group_by(SUPP_COLOR) %>%
     summarize(tot = n()) %>%
     arrange(-tot)
+summary(color)
 
 prop.table(table(hier$SIZE_1_IDNT))
-    
-ggplot(color, aes(x=tot))+geom_histogram(bins=10)
+
+style <- hier %>%
+        group_by(sb) %>%  #change name variable
+    summarize(tot = n()) %>%
+    arrange(-tot)
+summary(style)
+
+ggplot(style, aes(x=tot))+geom_histogram(bins=10)
 
 summary(color)
     
+prop.table(table(hier$NDIRECT_SIZE1))
+prop.table(table(hier$SUPP_NPG_CDE))*100
+
+hier2 <- hier %>%
+    mutate(aux = paste(SUPP_PRT_NBR, SUPP_COLOR, sep = "_")) %>%
+    filter(aux !=CC)
+
+
+
+#### BRANDS ###
+
+sku_price <- demand %>%
+    group_by(SKU_IDNT, UNIT_PRICE_DEMAND) %>%
+    summarize(total_units = sum(UNITS), total_demand = sum(DEMAND))%>%
+    arrange(-total_units) %>%
+    inner_join(description, by = "SKU_IDNT") %>%
+    mutate(PRICE = round(max(UNIT_PRICE_DEMAND, ORIG_PRC_AMT),2)) %>%
+    select(-c(UNIT_PRICE_DEMAND, ORIG_PRC_AMT)) %>%
+    group_by(SKU_IDNT) %>%
+    summarize(UNITS = sum(total_units), price = mean(PRICE))
+
+brands <- description %>%
+    select(SKU_IDNT, BRAND_NAME) %>%
+    inner_join(sku_price, by = "SKU_IDNT")
+
+ggplot(brands, aes(UNITS, price)) + geom_point(aes(color=BRAND_NAME)) + guides(colour=FALSE)
+
+color <- description %>%
+    select(SKU_IDNT, CLR_FMLY_DESC) %>%
+    inner_join(sku_price, by = "SKU_IDNT")
+
+ggplot(color, aes(log(UNITS), log(price))) + geom_point(aes(color=CLR_FMLY_DESC)) + guides(colour=FALSE)
