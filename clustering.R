635597eb37df75671fca31210ff3899d8fe9642b
Lore <lors2404@gmail.com>
2017-04-11 16:37:34 -0400

    clustering brands

--- clustering.R
+++ clustering.R
@@ -13,25 +13,29 @@ library(rpart.plot)
 library(sqldf)
 library(tidyr)
 library(qcc)
-library(fastcluster)
+library(caret)
+library(ggdendro)
 
 ####################################   Read/load data  ###############################################
 
 load('data/created/demand.Rdata')
 load('data/created/information.Rdata')
 
+#Filter price_types
 demand <- demand %>%
     filter(PRICE_TYPE %in% c('PROMO','REG')) 
 
 bridge <- demand %>%
-    filter(DEPT == 'Bridge')
+    filter(DEPT == 'Bridge') %>%
+    select(-DEPT)
 
-contemporary <- demand %>%
-    filter(DEPT == 'Contemporary')
+###### CREATE CLUSTERS FOR THE BRANDS  ######
 
-######
+# 119 brands in bridge
 
-brand <- bridge %>%
+#Time effects
+
+brand_time <- bridge %>%
     group_by(BRAND_NAME,DAY_DT) %>%
     summarise(UNITS = sum(UNITS)) %>%
     mutate(YEAR = year(DAY_DT)) %>%
@@ -42,25 +46,93 @@ brand <- bridge %>%
               END = max(DAY_DT),
               ACTIVE_DAYS = n())
 
-                            
-                  
+table(brand_time$YEAR)
 
-                  
-######                  
-    
-tot_units <- sum(demand$UNITS)
+brand_color1 <- bridge %>%
+    mutate(YEAR = year(DAY_DT)) %>%
+    filter(YEAR < 2017) %>%
+    group_by(BRAND_NAME, YEAR, COLOR) %>%
+    summarise(freq = n())
 
-brand1 <- bridge %>%
+brand_color <- brand_color1 %>%
+    left_join(brand_time %>% select(BRAND_NAME, YEAR, UNITS),by = c("BRAND_NAME", "YEAR")) %>%
+    mutate(rel_freq = freq/UNITS) %>%
+    select(-c(freq,UNITS)) %>%
+    spread(COLOR,rel_freq)
+
+brand_color[is.na(brand_color)] <- 0
+
+brand_style1 <- bridge %>%
+    mutate(YEAR = year(DAY_DT)) %>%
+    filter(YEAR < 2017) %>%
+    group_by(BRAND_NAME, YEAR, CLASS_DESC ) %>%
+    summarise(freq = n()) 
+
+brand_style <- brand_style1 %>%
+    left_join(brand_time %>% select(BRAND_NAME, YEAR, UNITS),by = c("BRAND_NAME", "YEAR")) %>%
+    mutate(rel_freq = freq/UNITS) %>%
+    select(-c(freq,UNITS)) %>%
+    spread(CLASS_DESC,rel_freq)
+
+brand_style[is.na(brand_style)] <- 0
+
+brand_info <- description %>%
+    filter(DEPT == 'Bridge') %>% 
     group_by(BRAND_NAME) %>%
-    summarise(BRAND_FREQ = sum(UNITS))
+    summarise(CREATE = min(CREATE_DT),
+              CREATE = as.Date(ifelse(is.na(CREATE), min(LOAD_DT), CREATE)))
 
-color <- bridge %>%
-    group_by(COLOR) %>%
-    summarise(COL_FREQ = sum(UNITS))
+elapsed_months <- function(end_date, start_date) {
+    ed <- as.POSIXlt(end_date)
+    sd <- as.POSIXlt(start_date)
+    12 * (ed$year - sd$year) + (ed$mon - sd$mon)
+}
+
+brand <- brand_time %>%
+    left_join(brand_info, by = "BRAND_NAME") %>%
+    mutate(seniority = elapsed_months(END, CREATE)) %>%
+    left_join(brand_color, by = c("BRAND_NAME", "YEAR")) %>%
+    left_join(brand_style, by = c("BRAND_NAME", "YEAR")) %>%
+    select(-c(START,END,CREATE))
+
+######    CLUSTERING    #######
+
+preproc = preProcess(brand[-c(1:2)])
+brandNorm = predict(preproc, brand[-c(1:2)])
+summary(brandNorm)
+
+d <- dist(brandNorm)
+clusters <- hclust(d, method = "ward.D")
+plot(clusters, hang = -1)
+clusterGroups = cutree(clusters, k = 5)
+rect.hclust(clusters, k=5, border="red")
+
+table(clusterGroups)
+tapply(brand$UNITS, clusterGroups, mean)
+tapply(brand$seniority,clusterGroups, mean)
+tapply(brand$SHOULDER,clusterGroups, mean)
+tapply(brand$ACTIVE_DAYS,clusterGroups, mean)
+
+brand$clust <-  clusterGroups
+
+######   MODEL     #######
+
+bridge <- bridge %>% 
+    mutate(YEAR = year(DAY_DT)) %>%
+    left_join(brand %>% select(BRAND_NAME, YEAR, clust),by = c("BRAND_NAME", "YEAR"))
+
+control = rpart.control(cp = 0.01, minsplit = )
+tree <- rpart(UNITS ~  N_BRAND + CLASS_DESC + EVENT + month(DAY_DT) + UNIT_PRICE_DEMAND + COLOR + clust, 
+              data = bridge, method = 'anova', minsplit = 10000)
+rpart.plot(tree)
+
+
+
+
+########################
 
 sku_info <- bridge %>%
     left_join(brand, by ="BRAND_NAME") %>%
-    left_join(color, by ="COLOR") %>%
     group_by(SKU_IDNT,COLOR,BRAND_NAME) %>%
     summarise(units = sum(UNITS),
               mean_price = mean(UNIT_PRICE_DEMAND),
@@ -107,48 +179,5 @@ information <- description %>%
     inner_join(sku_price, by = c("SKU_IDNT", "ORIG_PRICE"))
 
 
-## brand
 
-brand_info <- bridge %>%
-    group_by(BRAND_NAME,DAY_DT) %>%
-    summarise(UNITS = sum(UNITS)) %>%
-    mutate(YEAR = year(DAY_DT)) %>%
-    filter(YEAR < 2017) %>%
-    group_by(BRAND_NAME, YEAR) %>%
-    summarise(UNITS = sum(UNITS),
-              START = min(DAY_DT),
-              END = max(DAY_DT),
-              ACTIVE_DAYS = n())
-
-brand_info2 <- description %>%
-    filter(DEPT == 'Bridge') %>% 
-    group_by(BRAND_NAME) %>%
-    summarise(LOAD = min(LOAD_DT),
-              CLOSE = max(CLOSE_DT),
-              CREATE = min(CREATE_DT))
-
-brand_join <- brand_info %>%
-    left_join(brand_info2, by = "BRAND_NAME")
-
-brand <- brand_join %>%
-    mutate(BEG = as.Date(ifelse(year(LOAD)==YEAR, 
-                                pmin(START,LOAD), 
-                                pmin(START,as.Date(paste(as.character(YEAR),'-01-01',sep=''))))),
-           END = as.Date(ifelse(year(CLOSE)==YEAR,
-                                pmax(END,CLOSE),
-                                pmax(END,as.Date(paste(as.character(YEAR),'-12-31',sep=''))))),
-           LENGTH = as.numeric(END-BEG+1)) %>%
-    select(BRAND_NAME, UNITS, BEG, END, LENGTH) 
-
-d <- dist(brand[,c("UNITS","LENGTH")])
-
-clusters <- hclust(d, method = "ward.D")
-
-plot(clusters)
-clusterGroups = cutree(clusters, k = 5)
-table(clusterGroups)
-tapply(brand$TOT_UNITS, clusterGroups, mean)
-tapply(brand$LENGTH, clusterGroups, mean)
-
-brand$clust <-  clusterGroups
 
