76f4543494d1b689e846bc26426e7b70b5aff822
Lore <lors2404@gmail.com>
2017-05-11 12:28:30 -0400

    improvements in model accuracy

--- model.R
+++ model.R
@@ -8,37 +8,40 @@ load("data/created/time_series.Rdata")
 load("data/created/brand_info_bridge")
 load("data/created/information.Rdata")
 
-description <- description %>%
-    left_join(brand %>% select(BRAND_NAME, clust)) 
-
 aux <- time_series %>%
     arrange(SKU_IDNT, YEAR, MTH_IDNT) %>%
     group_by(SKU_IDNT) %>%
-    mutate(lagged_1 = lag(UNITS, 9),
-           lagged_2 = lag(UNITS, 10),
-           lagged_3 = lag(UNITS, 11),
-           lagged_4 = lag(UNITS, 12),
-           lagged_5 = lag(UNITS, 13)) %>%
     left_join(description %>% select(SKU_IDNT, BRAND_NAME, N_BRAND, CLASS_DESC, ORIG_PRICE, COLOR),
               by = "SKU_IDNT") %>%
     left_join(brand %>% select(BRAND_NAME, YEAR, clust), by = c("YEAR", "BRAND_NAME"))
 
 aux2 <- aux %>%
     group_by(clust, COLOR, CLASS_DESC, YEAR, MTH_IDNT) %>%
-    summarise(UNITS= sum(UNITS)) %>%
+    summarise(UNITS= sum(UNITS)) 
+
+aux3 <- aux2 %>%
     arrange(clust, COLOR, CLASS_DESC, YEAR, MTH_IDNT) %>%
+    group_by(clust, COLOR, CLASS_DESC) %>%
     mutate(lagged_1 = lag(UNITS, 9),
            lagged_2 = lag(UNITS, 10),
            lagged_3 = lag(UNITS, 11),
            lagged_4 = lag(UNITS, 12),
            lagged_5 = lag(UNITS, 13)) 
 
-model1 <- lm(UNITS ~ lagged_1 + lagged_2 + lagged_3, data = subset(aux, clust == 5))
+### REGRESSION MODEL ####
+
+#All units 
+
+model1 <- lm(UNITS ~ 0 + lagged_1 + lagged_2 + lagged_3 + lagged_4 + 
+                 as.factor(MTH_IDNT), data = subset(aux3,clust==5))
 summary(model1)
 
-tree <- rpart(UNITS ~ lagged_1 + lagged_2 + lagged_3 + lagged_4 + lagged_5+ 
-                  COLOR + ORIG_PRICE + 
-                  as.factor(MTH_IDNT), data = subset(aux, clust == 5), method = 'anova')
+
+
+control <- rpart.control(minsplit = 1500 )
+tree <- rpart(UNITS ~ lagged_1 + lagged_2 + lagged_3 + lagged_4 + 
+               as.factor(MTH_IDNT), data = aux3, method = 'anova',
+              control = control)
 rpart.plot(tree)
 
 
